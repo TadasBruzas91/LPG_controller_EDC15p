ca870a3406b2860c97e4334ad24ff1dae803b787
Tadas Bružas <tadasbruzas91@gmail.com>
2022-03-15 23:15:12 +0200

    Add variables

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,30 @@
 #define CAN0_INT 2
 MCP_CAN CAN0(10);
 
+// Setup
+const uint16_t min_lpg_rpm = 950;
+const uint16_t max_lpg_rpm = 3500;
+const uint8_t min_reducer_temp = 30;
+const uint16_t max_lpg_egt_temp = 500;
+const uint8_t lpg_injection_percentage = 10;
+
 // CANBUS read variables
 long unsigned int can_id;
 unsigned char len = 0;
 unsigned char can_message[8];
+
+// Engine variables 
+uint16_t rpm, iq_diesel, iq_lpg, egt_temp;
+uint8_t tps;
+
+// LPG variables
+uint8_t reducer_temp, lpg_temp;
+uint16_t map_pressure, lpg_pressure;
+bool lpg_switch = false;
+
+void injection(){
+  
+}
 
 void read_canbus(){
   if(!digitalRead(CAN0_INT)){
