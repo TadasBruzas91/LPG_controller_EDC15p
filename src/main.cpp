3b4cc1eb95013ba47e537e92e2fd4253a23ca854
Tadas Bružas <tadasbruzas91@gmail.com>
2022-03-16 12:29:17 +0200

    Add injection logic.

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,14 @@
 MCP_CAN CAN0(10);
 
 // Setup
-const uint16_t min_lpg_rpm = 950;
-const uint16_t max_lpg_rpm = 3500;
-const uint8_t min_reducer_temp = 30;
-const uint16_t max_lpg_egt_temp = 500;
-const uint8_t lpg_injection_percentage = 10;
+const uint16_t MIN_LPG_RPM = 950; // RPM
+const uint16_t MAX_LPG_RPM = 3500; // RPM
+const uint8_t MIN_REDUCER_TEMP = 30; // Celcius
+const uint16_t MAX_LPG_EGT_TEMP = 500; // Celcius
+const uint8_t LPG_INJECTION_PERCENTAGE = 10; // %
+const uint8_t LPG_INJECTOR_PIN = 3;
+const uint16_t LPG_INJECTOR_OPEN_TIME = 3200; // microseconds
+const uint16_t LPG_INJECTOR_OPEN_TIME = 2000; // microseconds
 
 // CANBUS read variables
 long unsigned int can_id;
@@ -24,11 +27,30 @@ uint8_t tps;
 // LPG variables
 uint8_t reducer_temp, lpg_temp;
 uint16_t map_pressure, lpg_pressure;
+uint16_t lpg_inj_duration;
 bool lpg_switch = false;
 
-void injection(){
-  
+// Injection //
+void calculate_inj_duration(){
+
 }
+
+void open_injector(){
+
+}
+
+void close_injector(){
+
+}
+
+void injection(){
+  if(lpg_switch && reducer_temp >= MIN_REDUCER_TEMP && rpm >= MIN_LPG_RPM && rpm <= MAX_LPG_RPM && egt_temp <= MAX_LPG_EGT_TEMP){
+    iq_lpg = iq_diesel * LPG_INJECTION_PERCENTAGE / 100;
+    calculate_inj_duration();
+    open_injector();
+  }
+}
+// Injection //
 
 void read_canbus(){
   if(!digitalRead(CAN0_INT)){
@@ -48,5 +70,6 @@ void setup() {
 }
 
 void loop() {
+  close_injector();
   
 }
