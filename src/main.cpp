28df4826c0bab863fff58bf166419f4a75a45784
Tadas Bružas <tadasbruzas91@gmail.com>
2022-03-16 13:44:04 +0200

    Add sensors read logic.

--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,52 @@
 #define CAN0_INT 2
 MCP_CAN CAN0(10);
 
-// Setup
+// Setup //
 const uint16_t MIN_LPG_RPM = 950; // RPM
 const uint16_t MAX_LPG_RPM = 3500; // RPM
 const uint8_t MIN_REDUCER_TEMP = 30; // Celcius
 const uint16_t MAX_LPG_EGT_TEMP = 500; // Celcius
 const uint8_t LPG_INJECTION_PERCENTAGE = 10; // %
 const uint8_t LPG_INJECTOR_PIN = 3;
+const uint8_t DIESEL_INJECTOR_INPUT_OUTPUT = 4;
 const uint16_t LPG_INJECTOR_OPEN_TIME = 3200; // microseconds
-const uint16_t LPG_INJECTOR_OPEN_TIME = 2000; // microseconds
+const uint16_t LPG_INJECTOR_CLOSE_TIME = 2000; // microseconds
 
-// CANBUS read variables
+// CANBUS read variables //
 long unsigned int can_id;
-unsigned char len = 0;
+unsigned char can_msg_len = 0;
 unsigned char can_message[8];
 
-// Engine variables 
+// Engine variables //
 uint16_t rpm, iq_diesel, iq_lpg, egt_temp;
 uint8_t tps;
 
-// LPG variables
+// LPG variables //
 uint8_t reducer_temp, lpg_temp;
 uint16_t map_pressure, lpg_pressure;
 uint16_t lpg_inj_duration;
 bool lpg_switch = false;
 
-// Injection //
+// All controller sensors //
+void read_map(){
+
+}
+
+void read_lpg_temp(){
+
+}
+
+void read_reducer_temp(){
+
+}
+
+void read_sensors(){
+
+}
+
+// All controller sensors //
+
+// Injection start //
 void calculate_inj_duration(){
 
 }
@@ -50,11 +70,11 @@ void injection(){
     open_injector();
   }
 }
-// Injection //
+// Injection end//
 
 void read_canbus(){
   if(!digitalRead(CAN0_INT)){
-    CAN0.readMsgBuf(&can_id, &len, can_message);
+    CAN0.readMsgBuf(&can_id, &can_msg_len, can_message);
   }
 }
 
@@ -71,5 +91,5 @@ void setup() {
 
 void loop() {
   close_injector();
-  
+  read_canbus();
 }
